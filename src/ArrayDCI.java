b42610c05c5dd03fbcd4d577d57dfc9b59e1e507
Vedant Agrawal <vedantagrawal647@gmail.com>
2021-09-21 14:10:53 +0530

    Initial commit

---
+++ src/ArrayDCI.java
@@ -0,0 +1,13 @@
+public class ArrayDCI {
+    public static void main(String[] args){
+        int arr[]=new int[3];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+
+        for(int i:arr)
+        {
+            System.out.println(i);
+        }
+    }
+}
